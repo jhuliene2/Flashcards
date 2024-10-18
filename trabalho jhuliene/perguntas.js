58a4552116ab06c319259c6c4e37b6553f662dc9
jhuliene2 <160598116+jhuliene2@users.noreply.github.com>
2024-10-18 19:47:25 -0300

    Add files via upload

---
+++ trabalho jhuliene/perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Biotecnologia',
+    'O que é Genetica?',
+    'é uma ciencia que faz parte da biologia. Ela tem como objetivo compreender a transmissão das carcterisiticas dos seres vivos para seu descendentes, ou seja, a hereditariedade'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'A capital do canadá é ottawa'
+)
+
+criaCartao(
+    'Biologia',
+    'O fungo é um ser vivo ou uma bacteria?',
+    'fungos são seres vivos heterotroficos, ou seja, que precisam obter materiais orgânicos já formados para sobreviver, não sendocapazes de produzirem seu próprio alimento'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como jango?',
+    'João Goulart'
+)
+criaCartao(
+    'Quimica',
+    'Atualmente, quantos elementos químicos a tabela periodica possui?',
+    '118'
+)
+criaCartao(
+    'geografia',
+    'Quais os países que tem a maior e a menor expectativa de vidado mundo?',
+    'Japão e Serra Leoa'
+)
